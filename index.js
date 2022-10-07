0a34847e06fc2ad76775436487adc70c2dccbe4a
Sondos Ahmed Ibrahim <sunnysondos42@gmail.com>
2022-10-07 13:27:03 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+
+class Polygon {
+    constructor(arr) {
+        this.arr = arr
+    }
+    get countSides() {
+        return this.arr.length;
+    }
+    get perimeter() {
+        return this.arr.reduce((total, el) => el + total);
+    }
+}
+class Triangle extends Polygon {
+    constructor(arr) {
+        super(arr);
+    }
+    get isValid() {
+        return ((this.arr[2]+this.arr[1])>this.arr[0]);
+    }
+}
+class Square extends Polygon {
+    constructor(arr) {
+        super(arr);
+    }
+    get isValid() {
+        return this.arr.length === 4 && this.arr[0] === this.arr[1] && this.arr[2] === this.arr[3];
+    }
+    get area() {
+        return this.arr[0] ** 2;
+    }
+}
